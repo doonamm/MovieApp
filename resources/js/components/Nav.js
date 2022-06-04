d43c87531dea01ee66c1c16fd712df7e03fb93b8
LeeAhnTheu <anhthu88292@gmail.com>
2022-06-05 01:27:05 +0700

    Thu - update nav

--- resources/js/components/Nav.js
+++ resources/js/components/Nav.js
@@ -15,29 +15,36 @@ function Nav(props){
     return(
         <div className="nav">
             <div className="wrap-center">
-                <div className="logo img-wrapper">
-                    <img src="https://previews.123rf.com/images/michaelrayback/michaelrayback1610/michaelrayback161000022/64360128-.jpg"/>
-                </div>
-                <ul>
-                    <li>Home</li>
-                    <li>Movies</li>
-                    <li>Collections</li>
-                </ul>
-                <form className="search">
-                    <input type="text"/>
-                    <button type="submit"><GiMagnifyingGlass/></button>
-                </form>
-                <div className="user-controllers">
-                    <div onClick={toggleController} className="img-wrapper">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png"/>
+                <div className='left'>
+                    <div className="logo img-wrapper">
+                        <img src="https://previews.123rf.com/images/michaelrayback/michaelrayback1610/michaelrayback161000022/64360128-.jpg"/>
                     </div>
-                    {open && (
-                        <ul className="controllers">
+                    <ul className='nav-list'>
+                        <li>Home</li>
+                        <li>Movies</li>
+                        <li>Collections</li>
+                    </ul>
+                </div>
+                <div className='right'>
+                    <form className="search">
+                        <input type="text" placeholder="Search film..."/>
+                        <button type="submit"><GiMagnifyingGlass/></button>
+                    </form>
+                    <div className="user-controllers">
+                        <div onClick={toggleController} className="img-wrapper avatar">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png"/>
+                        </div>
+                        {/* 
+                            ---- Todo ----
+                            Announce component
+                        
+                        */}
+                        <ul className={open ? "controllers show" : "controllers"}>
                             <li>Settings</li>
                             <li>Edit profile</li>
                             <li>Sign out</li>
                         </ul>
-                    )}
+                    </div>
                 </div>
             </div>
         </div>
