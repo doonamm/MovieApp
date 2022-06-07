b5e2b10e9b044c408a695ff5396b14ba383456da
CuCaRot <namkienlua@gmail.com>
2022-06-07 23:42:23 +0700

    Nam - update search movie

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -87,9 +87,9 @@ class Handler extends ExceptionHandler
             
 
             //------------end modify
-            case $e instanceof Exception:
-                $errorMsg = 'Something went wrong';
-                break;
+            // case $e instanceof Exception:
+            //     $errorMsg = 'Something went wrong';
+            //     break;
         }
 
         if(strlen($errorMsg)>0){
