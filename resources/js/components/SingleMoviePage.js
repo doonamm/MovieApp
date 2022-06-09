a067b0067605a249f853fcedd61c2d94eb83b2f5
Lê Quang Nhật <quangnhatle2601@gmail.com>
2022-06-09 14:05:44 +0700

    commit

--- resources/js/components/SingleMoviePage.js
+++ resources/js/components/SingleMoviePage.js
@@ -1,8 +1,8 @@
-import {GiPlayButton} from 'react-icons/gi';
-import {FaPlus} from 'react-icons/fa';
-import  '../../style/SingleMoviePage.scss';
+import { GiPlayButton } from 'react-icons/gi';
+import { FaPlus } from 'react-icons/fa';
+import '../../style/SingleMoviePage.scss';
 function GiaoDienPhim(props) {
-    return(
+    return (
         <div className="page giaodien_phim">
             <div className="container margin">
                 <h2 className="name ">Moive Name</h2>
@@ -11,11 +11,11 @@ function GiaoDienPhim(props) {
                     <button> 1h 20m</button>
                 </div>
                 <p className="description khoangcach">
-                Harry Potter là tên của series tiểu thuyết phim huyền bí gồm bảy phần của nhà văn Anh Quốc J. K. Rowling. Bộ truyện viết về những cuộc phiêu lưu phù thủy của cậu bé Harry Potter cùng hai người bạn thân là Ronald Weasley và Hermione Granger, lấy bối cảnh tại Trường Phù thủy và Pháp sư Hogwarts nước Anh.
+                    Harry Potter là tên của series tiểu thuyết phim huyền bí gồm bảy phần của nhà văn Anh Quốc J. K. Rowling. Bộ truyện viết về những cuộc phiêu lưu phù thủy của cậu bé Harry Potter cùng hai người bạn thân là Ronald Weasley và Hermione Granger, lấy bối cảnh tại Trường Phù thủy và Pháp sư Hogwarts nước Anh.
                 </p>
                 <div className="direction_button khoangcach">
-                    <button><span className='btn_icon'><GiPlayButton/></span>Play</button>
-                    <button><span className='btn_icon'><FaPlus/></span>Add to my list</button>
+                    <button><span className='btn_icon'><GiPlayButton /></span>Play</button>
+                    <button><span className='btn_icon'><FaPlus /></span>Add to my list</button>
                 </div>
                 <div className='moredescription khoangcach'>
                     <p className='title'> More description</p>
@@ -33,7 +33,7 @@ function GiaoDienPhim(props) {
                     <div className='movie_list'>
                         <div>
                             <div className='movie'>
-                                <img src="https://img.idesign.vn/2021/01/studio-ghibli-history-7.jpg"/>
+                                <img src="https://img.idesign.vn/2021/01/studio-ghibli-history-7.jpg" />
                             </div>
                             <span>Movie name</span>
                         </div>
